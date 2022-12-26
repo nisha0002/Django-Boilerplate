6cc32abb0b2ae64f3746f7ff6436d5380fd79da3
nisha0002 <121237327+nisha0002@users.noreply.github.com>
2022-12-26 15:39:55 +0530

    Update urls.py

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -13,49 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from multiprocessing import AuthenticationError
-from re import template
-from django.conf import settings
-from django.urls import path
-from app import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from .forms import LoginForm, MyPasswordChangeForm
-
-
+from django.contrib import admin
+from django.urls import path , include
 urlpatterns = [
-    #path('', views.home),
-    path('',views.ProductView.as_view(),name="home"),
-    path('product-detail/<int:pk>',views.ProductDetailView.as_view(),name="product-detail"),
-    
-    path('checkout/', views.checkout, name='checkout'),
-    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
-    path('cart/',views.show_cart, name='showcart'),
-    path('pluscart/',views.plus_cart),
-    path('minuscart/',views.minus_cart),
-    path('removecart/',views.remove_cart),  
-    path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('address/', views.address, name='address'),
-    path('orders/', views.orders, name='orders'),
-    
-    path('mobile/', views.mobile, name='mobile'),
-    path('mobile/<slug:data>', views.mobile, name='mobiledata'),
-    path('laptop/', views.laptop, name='laptop'),
-    path('laptop/<slug:data>', views.laptop, name='laptopdata'),
-    path('topwears/', views.topwears, name='topwears'),
-    path('topwears/<slug:data>', views.topwears, name='topwearsdata'),
-    path('bottomwears/', views.bottomwears, name='bottomwears'),
-    path('bottomwears/<slug:data>', views.bottomwears, name='bottomwearsdata'),
-    path('accounts/login/',auth_views.LoginView.as_view(template_name='app/login.html', authentication_form =LoginForm),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm , success_url='/passwordchangedone/'), name='passwordchange'),
-    path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html'), name='passwordchangedone'),
-    #path('login/', views.login, name='login'),
-    #path('registration/', views.customerregistration, name='customerregistration'),
-    path('registration/',views.CustomerRegistrationView.as_view(), name='customerregistration'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('paymentdone/', views.payment_done, name='paymentdone'),
-] +  static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('',include('app.urls'))
 
